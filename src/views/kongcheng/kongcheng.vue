161311710acd26e3f40d04cb3981cfb24539f3a4
望仔 <3545684577@qq.com>
2021-12-26 20:03:32 +0800

    提交了空乘

---
+++ src/views/kongcheng/kongcheng.vue
@@ -0,0 +1,276 @@
+<template>
+  <div class="mian">
+    <div class="banner">
+      <img src="../../assets/img/空乘/images/航空.jpg" alt="" />
+    </div>
+    <div class="conter">
+      <div class="head">
+        <p>当前位置：首页>热门专业推荐>航空专业</p>
+      </div>
+      <div class="conter_nei">
+        <div class="conter_title">专业介绍</div>
+        <div class="conter_two">
+          <div class="contwo_left">
+            <img src="../../assets/img/空乘/images/空乘专业pc_03.jpg" alt="" />
+          </div>
+          <div class="contwo_right">
+            <ul class="contri_ul">
+              <li>
+                <span class="hong">空乘专业:</span>
+                是培养适应21世纪民航运输事业发展，具有坚实的英语基
+                础、突出英语应用能力，较高的人文素质，受过专门航空运输技能
+                知识训练的实用型民航高级空中乘务员...
+              </li>
+              <li>
+                <span class="lan">空乘专业:</span>
+                是培养适应21世纪民航运输事业发展，具有坚实的英语基
+                础、突出英语应用能力，较高的人文素质，受过专门航空运输技能
+                知识训练的实用型民航高级空中乘务员...
+              </li>
+              <li>
+                <span class="lv">目前我国:</span>
+                的很多高铁院校都是与企业实行资源互补，企合作的发展方
+                向和趋势，高铁乘务专业的学生在毕业之前学校一般会提供实习机会，
+                在高铁企业实习期间，表现突出的学生会有优先就业的机会...
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="conter_nei">
+        <div class="conter_title">专业优势</div>
+        <div class="conter_two">
+          <div class="contwo_right">
+            <ul class="contri_ul">
+              <li>
+                <span class="hong">空乘专业:</span>
+                是培养适应21世纪民航运输事业发展，具有坚实的英语基
+                础、突出英语应用能力，较高的人文素质，受过专门航空运输技能
+                知识训练的实用型民航高级空中乘务员...
+              </li>
+              <li>
+                <span class="lan">空乘专业:</span>
+                是培养适应21世纪民航运输事业发展，具有坚实的英语基
+                础、突出英语应用能力，较高的人文素质，受过专门航空运输技能
+                知识训练的实用型民航高级空中乘务员...
+              </li>
+              <li>
+                <span class="lv">目前我国:</span>
+                的很多高铁院校都是与企业实行资源互补，企合作的发展方
+                向和趋势，高铁乘务专业的学生在毕业之前学校一般会提供实习机会，
+                在高铁企业实习期间，表现突出的学生会有优先就业的机会...
+              </li>
+            </ul>
+          </div>
+          <div class="contwo_left">
+            <img src="../../assets/img/空乘/images/空乘专业pc_07.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+      <div class="conter_nei">
+        <div class="conter_title">职业规划</div>
+        <div class="conter_two">
+          <div class="contwo_left">
+            <img src="../../assets/img/空乘/images/空乘专业pc_11.jpg" alt="" />
+          </div>
+          <div class="contwo_right">
+            <ul class="contri_ul">
+              <li>
+                <span class="hong">空乘专业:</span>
+                是培养适应21世纪民航运输事业发展，具有坚实的英语基
+                础、突出英语应用能力，较高的人文素质，受过专门航空运输技能
+                知识训练的实用型民航高级空中乘务员...
+              </li>
+              <li>
+                <span class="lan">空乘专业:</span>
+                是培养适应21世纪民航运输事业发展，具有坚实的英语基
+                础、突出英语应用能力，较高的人文素质，受过专门航空运输技能
+                知识训练的实用型民航高级空中乘务员...
+              </li>
+              <li>
+                <span class="lv">目前我国:</span>
+                的很多高铁院校都是与企业实行资源互补，企合作的发展方
+                向和趋势，高铁乘务专业的学生在毕业之前学校一般会提供实习机会，
+                在高铁企业实习期间，表现突出的学生会有优先就业的机会...
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="conter_he">
+        <div class="conter_title">合作单位</div>
+        <div class="cont_he">
+          <ul class="conthe_ul">
+            <li>
+              <img
+                src="../../assets/img/空乘/images/空乘专业pc_15.jpg"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="../../assets/img/空乘/images/空乘专业pc_18.jpg"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="../../assets/img/空乘/images/空乘专业pc_21.jpg"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="../../assets/img/空乘/images/空乘专业pc_27.jpg"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="../../assets/img/空乘/images/空乘专业pc_30.jpg"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="../../assets/img/空乘/images/空乘专业pc_32.jpg"
+                alt=""
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style scoped lang="scss">
+ul {
+  list-style: none;
+  padding-inline-start: 0;
+  margin-block-start: 0;
+  margin-block-end: 0;
+}
+.main {
+  width: 1920px;
+}
+.banner {
+  width: 1920px;
+  height: 230px;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+.conter {
+  width: 1200px;
+  margin: 0 auto;
+
+  .head {
+    width: 1200px;
+    height: 28px;
+    margin: 15px 0 46px 0;
+    text-align: left;
+    p {
+      color: rgb(136, 136, 136);
+      font-size: 14px;
+    }
+  }
+}
+.conter_nei {
+  width: 1200px;
+  height: 600px;
+
+  .conter_title {
+    width: 1200px;
+    height: 50px;
+    margin: 31px 0;
+    text-align: center;
+    font-weight: 600;
+    color: #1d6aa3;
+    font-size: 34px;
+    float: left;
+  }
+}
+.conter_two {
+  width: 1200px;
+  float: left;
+  .contwo_left {
+    width: 600px;
+    height: 400px;
+    float: left;
+    margin-left: 10px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .contwo_right {
+    width: 530px;
+    height: 400px;
+    float: left;
+    margin-left: 20px;
+    .contri_ul {
+      width: 530px;
+      li {
+        width: 530px;
+        height: 110px;
+        font-size: 17px;
+        font-weight: normal;
+        line-height: 36px;
+        margin-bottom: 10px;
+        text-align: left;
+      }
+      .hong {
+        color: #ff0e00;
+      }
+      .lan {
+        color: #006699;
+      }
+      .lv {
+        color: #009900;
+      }
+    }
+  }
+}
+.conter_he {
+  width: 1200px;
+  height: 734px;
+  .conter_title {
+    width: 1200px;
+    height: 50px;
+    margin: 31px 0;
+    text-align: center;
+    font-weight: 600;
+    color: #1d6aa3;
+    font-size: 34px;
+    float: left;
+  }
+  .conthe_ul {
+    width: 1200px;
+    float: left;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    li {
+      width: 370px;
+      height: 260px;
+      border: 1px solid;
+      float: left;
+      margin-bottom: 10px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      img {
+        width: 228px;
+        height: 157px;
+        text-align: center;
+        line-height: 40px;
+      }
+    }
+  }
+}
+</style>
