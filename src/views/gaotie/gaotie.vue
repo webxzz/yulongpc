a63418e1658b16c1e4e49148788468f022db504e
玥 <648355240>
2021-12-27 08:47:17 +0800

    提交了高铁

---
+++ src/views/gaotie/gaotie.vue
@@ -0,0 +1,272 @@
+<template>
+    <div id="gaotie">
+        <div class="nav">
+
+        </div>
+        <div class="main">
+            <div class="lunbo">
+                <div class="swiper mySwiper">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <img src="../../assets/img/高铁/images/高铁.jpg" alt="">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="../../assets/img/高铁/images/高铁.jpg" alt="">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="../../assets/img/高铁/images/高铁.jpg" alt="">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="../../assets/img/高铁/images/高铁.jpg" alt="">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="../../assets/img/高铁/images/高铁.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="swiper-button-next"></div>
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-pagination"></div>
+                </div>
+            </div>
+            <div class="main-nav">
+                当前位置：首页>热门专业推荐>高铁乘务
+            </div>
+            <div class="main-con">
+                <div class="content">
+                    <div class="con1">
+                        专业介绍
+                    </div>
+                    <div class="con2">
+                        <div class="c1">
+                            <img src="../../assets/img/高铁/images/高铁专业pc_03.jpg" alt="">
+                        </div>
+                        <div class="c2">
+                                <p>
+                                    <span style="color: rgb(255,51,0);">高铁乘务专业:</span>
+                                    全称是高速铁路客运乘务专业，是国家教 育部向铁路行业开设的高等职业教育专业。其专业学制为三年。就业主要集中在铁路旅客列车客运乘务及其相关方向...
+                                </p>
+                                <p>
+                                    <span style="color: rgb(0,102,153);">高铁乘务专业:</span>
+                                     是目前的一大热门专业，再加之中国目前的铁路系统技术和运营里程都是全世界规模最大的国家之一，因此我国的高铁行业发展会持续走高，高铁乘务专业的毕业生在就业前景和发展前景上是很光明的...
+                                </p>
+                                <p>
+                                    <span style="color: rgb(0,153,0);">目前我国:</span>
+                                    的很多高铁院校都是与企业实行资源互补，企合作的发展方向和趋势，高铁乘务专业的学生在毕业之前学校一般会提供实习机会，在高铁企业实习期间，表现突出的学生会有优先就业的机会...
+                                </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="con1">
+                        专业优势 
+                    </div>
+                    <div class="con2">
+                        <div class="c2">
+                                
+                            <p>
+                                <span style="color: rgb(255,51,0);">工作前景好:</span>
+                                我国的高铁事业也在不断的壮大，全国各地布满高铁，对于乘务员的需求也在不断地增加。选择高铁行业，会让您成就美好的未来...
+                            </p>
+                            
+                            <p>
+                                <span style="color: rgb(0,102,153);">人才需求量大:</span>
+                                我国高速铁路运营里程居世界第一位，正在建设之中的高速铁路有1万多公里。今后几年，我国高速铁路建设将进入全面收获时期。到2020年底至少需要新增加50000多名乘务员岗位...
+                            </p>
+                            
+                            <p>
+                                <span style="color: rgb(0,153,0);">薪资待遇好:</span>
+                                薪资由基本工资、出车次数、工作质量、列车效益、奖金、补助等构成。现在的乘务员薪水在五千到八千之间，如果你能晋升到中层的领导级别，可以拿到6位数的年薪。如果你升职到高层领导级别，薪资会更高。4.好就业优就业高铁乘务学校与多家用人单位签订合同，按照合作企业接下...
+                            </p>
+                        </div>
+                        <div class="c1">
+                            <img src="../../assets/img/高铁/images/高铁专业pc_07.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="con1">
+                        职业规划
+                    </div>
+                    <div class="con2">
+                        <div class="c1">
+                            <img src="../../assets/img/高铁/images/高铁专业pc_11.jpg" alt="">
+                        </div>
+                        <div class="c2">
+                            <p>
+                                <span style="color: rgb(255,51,0);">规划:</span>
+                                伴随着近几年铁路格局的改变，铁路事业的发展，形成了在铁路工程和服务领域的人才紧缺，这一专业的人才呈现出供不应求的状态，大量高铁乘务专业的人才缺口急需填补。因此，未来高铁专业的就业前景十分广阔...
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="con1">
+                        合作单位
+                    </div>
+                    <ul class="con-ul">
+                        <li class="cc">
+                            <img src="../../assets/img/高铁/images/高铁专业pc_15.jpg" alt="">
+                        </li>
+                        <li class="cc">
+                            <img src="../../assets/img/高铁/images/高铁专业pc_17.jpg" alt="">
+                        </li>
+                        <li class="cc">
+                            <img src="../../assets/img/高铁/images/高铁专业pc_20.jpg" alt="">
+                        </li>
+                        <li class="cc">
+                            <img src="../../assets/img/高铁/images/高铁专业pc_26.jpg" alt="">
+                        </li>
+                        <li class="cc">
+                            <img src="../../assets/img/高铁/images/高铁专业pc_28.jpg" alt="">
+                        </li>
+                        <li class="cc">
+                            <img src="../../assets/img/高铁/images/高铁专业pc_30.jpg" alt="">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Swiper from "swiper";
+import "swiper/css/swiper.css"
+export default {
+    mounted(){
+        var swiper = new Swiper(".mySwiper", {
+        cssMode: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        mousewheel: true,
+        keyboard: true,
+      });
+    },
+    methods: {
+        onChange(date, dateString) {
+        console.log(date, dateString);
+        },
+    },
+}
+</script>
+
+<style lang="scss">
+.gaotie{
+    width:100%;
+    height: 100%;
+}
+.nav{
+    width: 100%;
+    height: 200px;
+    background-color: #ddd;
+}
+.main{
+    width: 100%;
+}
+.lunbo{
+    position: relative;
+    width:1200px;
+    height: 460px;
+    top: 0;
+    margin: 0 auto;
+     .swiper {
+        width: 100%;
+        height: 100%;
+      }
+
+      .swiper-slide {
+        text-align: center;
+        font-size: 18px;
+        background: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      .swiper-slide img {
+        display: block;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+      .swiper-button-prev{
+          width: 25px;
+          height: 25px;
+          margin-left: 50px;
+      }
+      .swiper-button-next{
+          width: 25px;
+          height: 25px;
+          margin-right: 50px;
+      }
+}
+.main-nav{
+    width: 259px;
+    height: 14px;
+    font-size: 14px;
+    color: #888;
+    margin-left: 357px;
+    margin-top: 23px;
+    margin-bottom: 30px;
+}
+.main-con{
+    width: 1200px;
+    height: 100%;
+    margin: 0 auto;
+}
+.content{
+    height: 592px;
+    margin-bottom: 50px;
+    // background-color: #aaa;
+    padding: 20px;
+}
+.con1{
+    font-size: 30px;
+    font-weight: bold;
+    margin: 60px auto;
+    color: #1D6AA3;
+}
+.con2{
+    height: 400px;
+    display: flex;
+    flex-direction: row;
+}
+.c1{
+    width: 600px;
+    height: 400px;
+    margin: 10px;
+    img{
+        width: 100%;
+        height: 100%;
+    }
+}
+.c2{
+    width: 540px;
+    margin-left: 20px;
+    font-size: 18px;
+    line-height: 36px;
+    text-align: left;
+}
+.con-ul{
+    display: flex;
+    flex-wrap: wrap;
+}
+.cc{
+    display: flex;
+    width: 360px;
+    height: 200px;
+    border: 1px solid rgb(197,197,197);
+    margin: 10px;
+    // padding: 50px rgb(255,255,255);
+    img{
+        width: 60%;
+        height: 40%;
+        margin: 50px auto;
+    }
+}
+</style>
